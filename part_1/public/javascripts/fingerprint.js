e0dc2aa2036738e014750f4778bb085d82f412a3
Luis Molina <luismol@umich.edu>
2014-03-30 20:27:20 -0400

    Adding Heroku

--- part_1/public/javascripts/fingerprint.js
+++ part_1/public/javascripts/fingerprint.js
@@ -1,97 +1,4 @@
 
-/*
- * Returns an object with found plugins and their versions
- * Example: {
- *     flash: '10',
- *     pdf: '7',
- *     java: '6',
- *     silverlight: '5',
- *     quicktime: '7'
- * }
- */
-var plugins = (function(){
-    var found = {};
-    var version_reg = /[0-9]+/;
- 
-    /*
-     * Differentiate between IE (detection via ActiveXObject)
-     * and the rest (detection via navigator.plugins)
-     */
-    if (window.ActiveXObject) {
-        var plugin_list = {
-            flash: 'ShockwaveFlash.ShockwaveFlash.1',
-            pdf: 'AcroPDF.PDF',
-            silverlight: 'AgControl.AgControl',
-            quicktime: 'QuickTime.QuickTime'
-        }
- 
-        for (var plugin in plugin_list){
-            var version = msieDetect(plugin_list[plugin]);
-            if (version){
-                var version_reg_val = version_reg.exec(version);
-                found[plugin] = (version_reg_val && version_reg_val[0]) || '';
-            }
-        }
- 
-        if (navigator.javaEnabled()){
-            found['java'] = '';
-        }
-    } else {
-        var plugins = navigator.plugins;
-        console.log(plugins);
-        var reg = /Flash|PDF|Java|Silverlight|QuickTime/;
-        for (var i = 0; i < plugins.length; i++) {
-            var reg_val = reg.exec(plugins[i].description);
-            if (reg_val){
-                var plugin = reg_val[0].toLowerCase();
-                /*
-                 * Search in version property, if not available concat name
-                 *  and description and search for a version number in there
-                 */
-                var version = plugins[i].version || 
-                    (plugins[i].name + ' ' + plugins[i].description);
-                var version_reg_val = version_reg.exec(version);
-                if (!found[plugin]) {
-                    found[plugin] = (version_reg_val && version_reg_val[0]) || '';
-                }
-            }
-        }
-    }
- 
-    return found;
- 
-    /*
-     * Return version number if plugin installed
-     * Return true if plugin is installed but no version number found
-     * Return false if plugin not found
-     */ 
-    function msieDetect(name){
-        try {
-            var active_x_obj = new ActiveXObject(name);
-            try {
-                return active_x_obj.GetVariable('$version');
-            } catch(e) {
-                try {
-                    return active_x_obj.GetVersions();
-                } catch (e) {
-                    try {
-                        var version;
-                        for (var i = 1; i < 9; i++) {
-                            if (active_x_obj.isVersionSupported(i + '.0')){
-                                version = i;
-                            }
-                        }
-                        return version || true;
-                    } catch (e) {
-                        return true;
-                    }
-                }
-            }
-        } catch(e){
-            return false;
-        }
-    }
-})();
 
 userAgent_str = "<p>Browser CodeName: " + navigator.appCodeName + "</p>";
 userAgent_str+= "<p>Browser Name: " + navigator.appName + "</p>";
@@ -105,4 +12,34 @@ userAgent_str+= "<p>User-agent language: " + navigator.systemLanguage + "</p>";
 
 document.getElementById("example").innerHTML=userAgent_str;
 
-console.log(plugins);
+// var markers = [{ "position": "128.3657142857143", "markerPosition": "7" },
+//                { "position": "235.1944023323615", "markerPosition": "19" },
+//                { "position": "42.5978231292517", "markerPosition": "-3" }];
+
+var postPlugins = function() {	
+	var plugins = navigator.plugins;
+	var plugin_data = {};
+	console.log(plugins);
+	for(var i = 0; i < plugins.length; i++) {
+		var temp = 'plugin' + i;
+		plugin_data[temp] = "" + plugins[i].name + " " + plugins[i].description;				
+	}
+
+	console.log(plugin_data);
+
+	$.ajax({
+	    type: "POST",
+	    url: "/plugins",
+	    // The key needs to match your method's input parameter (case-sensitive).
+	    data: plugin_data,
+	    // contentType: "application/json; charset=utf-8",
+	    dataType: "JSON",
+	    success: function(data){alert(data);},
+	    failure: function(errMsg) {
+	        alert(errMsg);
+	    }
+	});
+
+};
+
+postPlugins();
--- part_1/routes/index.js
+++ part_1/routes/index.js
@@ -1,22 +1,56 @@
-
+var browser_fingerprints = {};
+var fingerprint = "";
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
-	console.log(req.headers);
+	// console.log(req.headers);	
+	var visit_count = 1;
+	if(fingerprint != "") {
+		// console.log("HEREEEE");
+		visit_count = browser_fingerprints[fingerprint];
+	}
 
-	var fingerprint = "";
+	res.render('index', 
+  	{ title: 'No-privacy',
+  		visits: visit_count
+  	});
+	// console.log(fingerprint);
+	// if(browser_fingerprints[])
+};
 
-	fingerprint += req.headers['user-agent'] + " ";
-	fingerprint += req.headers.accept + " ";
-	fingerprint += req.headers['accept-encoding'] + " ";
-	fingerprint += req.headers['accept-language'] + " ";
-
+exports.plugins = function(req, res) {
+	if(fingerprint == "") {
+		fingerprint += req.headers['user-agent'] + " ";
+		fingerprint += req.headers.accept + " ";
+		fingerprint += req.headers['accept-encoding'] + " ";
+		fingerprint += req.headers['accept-language'] + " ";	
+		var plugins = req.body;
+		console.log(plugins.length);
+		console.log(plugins);
+		var i = 0;
+		for(var plugin in plugins) {//i = 0; i < plugins.length; i++) {
+			var temp = 'plugin' + i++;
+			// console.log(plugins[temp]);
+			fingerprint += plugins[temp] + " ";
+		}
+		browser_fingerprints[fingerprint] = 2;	
+	}
+	else {
+		browser_fingerprints[fingerprint]++;
+	}
+	
+	console.log(browser_fingerprints[fingerprint]);	
+	
 	console.log(fingerprint);
 
-  res.render('index', 
-  	{ title: 'No-privacy',
-  		visits: 0
-  	});
+  // res.render('index', 
+  // 	{ title: 'No-privacy',
+  // 		visits: browser_fingerprints[fingerprint]
+  // 	});
+	// if(browser_fingerprints[fingerprint]) {
+	// 	browser_fingerprints[fingerprint]++;
+	// }
+	// else {
+	// }
 };
